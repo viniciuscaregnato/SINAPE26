bf7630bc80a12d0036e1b4aa6fb7dc89f68d3e03
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2026-04-06 22:03:11 -0300

    Add files via upload

---
+++ second-sample/functions/func-rf.R
@@ -0,0 +1,68 @@
+runrf=function(Y,indice,lag){
+  
+  dum=Y[1:(nrow(Y)-lag+1),ncol(Y)]
+  Y=Y[,-ncol(Y)]
+  comp=princomp(scale(Y,scale=FALSE))
+  Y2=cbind(Y,comp$scores[,1:4])
+  aux=embed(Y2,4+lag)
+  y=aux[,indice]
+  X=aux[,-c(1:(ncol(Y2)*lag))]  
+  
+  if(lag==1){
+    X.out=tail(aux,1)[1:ncol(X)]  
+  }else{
+    X.out=aux[,-c(1:(ncol(Y2)*(lag-1)))]
+    X.out=tail(X.out,1)[1:ncol(X)]
+  }
+  y = y[1:(length(y)-lag+1)]
+  X = X[1:(nrow(X)-lag+1),]
+  dum=tail(dum,length(y))
+  
+  model=randomForest(cbind(X,dum),y,importance = TRUE)
+  pred=predict(model,c(X.out,0))
+  
+  return(list("model"=model,"pred"=pred))
+}
+
+
+rf.rolling.window=function(Y,nprev,indice=1,lag=1){
+  
+  save.importance=list()
+  save.pred=matrix(NA,nprev,1)
+  for(i in nprev:1){
+    Y.window=Y[(1+nprev-i):(nrow(Y)-i),]
+    lasso=runrf(Y.window,indice,lag)
+    save.pred[(1+nprev-i),]=lasso$pred
+    save.importance[[i]]=importance(lasso$model)
+    cat("iteration",(1+nprev-i),"\n")
+  }
+  
+  real=Y[,indice]
+  plot(real,type="l")
+  lines(c(rep(NA,length(real)-nprev),save.pred),col="red")
+  
+  rmse=sqrt(mean((tail(real,nprev)-save.pred)^2))
+  mae=mean(abs(tail(real,nprev)-save.pred))
+  errors=c("rmse"=rmse,"mae"=mae)
+  
+  return(list("pred"=save.pred,"errors"=errors,"save.importance"=save.importance))
+}
+
+accumulate_model = function(forecasts){
+  
+  acc3 = c(rep(NA,2),sapply(1:(nrow(forecasts)-2), function(x){
+    prod(1+diag(forecasts[x:(x+2),1:3]))-1
+  })) 
+  acc6 = c(rep(NA,5),sapply(1:(nrow(forecasts)-5), function(x){
+    prod(1+diag(forecasts[x:(x+5),1:6]))-1
+  }))
+  acc12 = c(rep(NA,11),sapply(1:(nrow(forecasts)-11), function(x){
+    prod(1+diag(forecasts[x:(x+11),1:12]))-1
+  }))
+  
+  forecasts = cbind(forecasts,acc3,acc6,acc12)
+  colnames(forecasts) = c(paste("t+",1:12,sep = ""),"acc3","acc6","acc12")
+  
+  return(forecasts)
+  
+}
