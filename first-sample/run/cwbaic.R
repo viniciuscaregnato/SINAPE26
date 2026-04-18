326e0acbd9a66a1ab978414749f270321c66840b
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2026-04-18 19:25:05 -0300

    Add files via upload

---
+++ first-sample/run/cwbaic.R
@@ -0,0 +1,33 @@
+source("first-sample/functions/func-cwbaic.R")
+library(mboost)
+load("first-sample/data/rawdata.rda")
+Y=dados
+
+nprev=132
+
+## == passado == ##
+
+cwbaic1c=cwbaic.rolling.window(Y,nprev,1,1)
+cwbaic2c=cwbaic.rolling.window(Y,nprev,1,2)
+cwbaic3c=cwbaic.rolling.window(Y,nprev,1,3)
+cwbaic4c=cwbaic.rolling.window(Y,nprev,1,4)
+cwbaic5c=cwbaic.rolling.window(Y,nprev,1,5)
+cwbaic6c=cwbaic.rolling.window(Y,nprev,1,6)
+cwbaic7c=cwbaic.rolling.window(Y,nprev,1,7)
+cwbaic8c=cwbaic.rolling.window(Y,nprev,1,8)
+cwbaic9c=cwbaic.rolling.window(Y,nprev,1,9)
+cwbaic10c=cwbaic.rolling.window(Y,nprev,1,10)
+cwbaic11c=cwbaic.rolling.window(Y,nprev,1,11)
+cwbaic12c=cwbaic.rolling.window(Y,nprev,1,12)
+
+
+### == juntando tudo ==  ###
+
+forecast=cbind(cwbaic1c$pred,cwbaic2c$pred,cwbaic3c$pred,cwbaic4c$pred,
+               cwbaic5c$pred,cwbaic6c$pred,cwbaic7c$pred,cwbaic8c$pred,
+               cwbaic9c$pred,cwbaic10c$pred,cwbaic11c$pred,cwbaic12c$pred)
+
+forecast = accumulate_model(forecast)
+
+
+save(forecast,file="forecasts-samples/cwbaic/cwbaic-cpi1.rda")
