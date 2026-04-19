1092c176a03aea5ad3b7492b274aabb8aea8c6af
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2026-04-19 10:45:03 -0300

    Add files via upload

---
+++ first-sample/functions/func-xgb.R
@@ -0,0 +1,66 @@
+runxgb=function(Y,indice,lag){
+  
+  comp=princomp(scale(Y,scale=FALSE))
+  Y2=cbind(Y,comp$scores[,1:4])
+  aux=embed(Y2,4+lag)
+  y=aux[,indice]
+  X=aux[,-c(1:(ncol(Y2)*lag))]  
+  
+  if(lag==1){
+    X.out=tail(aux,1)[1:ncol(X)]  
+  }else{
+    X.out=aux[,-c(1:(ncol(Y2)*(lag-1)))]
+    X.out=tail(X.out,1)[1:ncol(X)]
+  }
+  y = y[1:(length(y)-lag+1)]
+  X = X[1:(nrow(X)-lag+1),]
+  
+  model = xgboost(X,label = y,nrounds = 1000, verbose = FALSE,
+                  params=list(eta=0.05,nthread=1,colsample_bylevel=2/3,subsample=1,max_depth=4,min_child_weigth=nrow(X)/200))
+  
+  pred=predict(model,t(X.out))
+  
+  return(list("model"=model,"pred"=pred))
+}
+
+
+xgb.rolling.window=function(Y,nprev,indice=1,lag=1){
+  
+  save.pred=matrix(NA,nprev,1)
+  for(i in nprev:1){
+    Y.window=Y[(1+nprev-i):(nrow(Y)-i),]
+    lasso=runxgb(Y.window,indice,lag)
+    save.pred[(1+nprev-i),]=lasso$pred
+    cat("iteration",(1+nprev-i),"\n")
+  }
+  
+  real=Y[,indice]
+  plot(real,type="l")
+  lines(c(rep(NA,length(real)-nprev),save.pred),col="red")
+  
+  rmse=sqrt(mean((tail(real,nprev)-save.pred)^2))
+  mae=mean(abs(tail(real,nprev)-save.pred))
+  errors=c("rmse"=rmse,"mae"=mae)
+  
+  return(list("pred"=save.pred,"errors"=errors))
+}
+
+
+accumulate_model = function(forecasts){
+  
+  acc3 = c(rep(NA,2),sapply(1:(nrow(forecasts)-2), function(x){
+    prod(1+diag(forecasts[x:(x+2),1:3]))-1
+  })) 
+  acc6 = c(rep(NA,5),sapply(1:(nrow(forecasts)-5), function(x){
+    prod(1+diag(forecasts[x:(x+5),1:6]))-1
+  }))
+  acc12 = c(rep(NA,11),sapply(1:(nrow(forecasts)-11), function(x){
+    prod(1+diag(forecasts[x:(x+11),1:12]))-1
+  }))
+  
+  forecasts = cbind(forecasts,acc3,acc6,acc12)
+  colnames(forecasts) = c(paste("t+",1:12,sep = ""),"acc3","acc6","acc12")
+  
+  return(forecasts)
+  
+}
