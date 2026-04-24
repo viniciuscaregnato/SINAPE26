45a2104ce0d1bb119d20b8cccd73976fbedda7bd
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2026-04-24 18:49:42 +0300

    Add files via upload

---
+++ second-sample/run/gbm-default-fixed.R
@@ -0,0 +1,40 @@
+source("second-sample/functions/func-gbm-default.R")
+library(HDeconometricsBeta)
+library(gbm)
+library(parallel)
+load("second-sample/data/rawdata.RData")
+Y=dados
+dum=rep(0,nrow(Y))
+dum[which.min(Y[,1])]=1
+Y=cbind(Y,dum=dum)
+
+nprev=180
+
+
+## == presente == ##
+
+gbm_default1c=gbm_default.rolling.window(Y,nprev,1,1)
+gbm_default2c=gbm_default.rolling.window(Y,nprev,1,2)
+gbm_default3c=gbm_default.rolling.window(Y,nprev,1,3)
+gbm_default4c=gbm_default.rolling.window(Y,nprev,1,4)
+gbm_default5c=gbm_default.rolling.window(Y,nprev,1,5)
+gbm_default6c=gbm_default.rolling.window(Y,nprev,1,6)
+gbm_default7c=gbm_default.rolling.window(Y,nprev,1,7)
+gbm_default8c=gbm_default.rolling.window(Y,nprev,1,8)
+gbm_default9c=gbm_default.rolling.window(Y,nprev,1,9)
+gbm_default10c=gbm_default.rolling.window(Y,nprev,1,10)
+gbm_default11c=gbm_default.rolling.window(Y,nprev,1,11)
+gbm_default12c=gbm_default.rolling.window(Y,nprev,1,12)
+
+### == juntando tudo ==  ###
+
+forecast=cbind(gbm_default1c$pred,gbm_default2c$pred,gbm_default3c$pred,gbm_default4c$pred,
+               gbm_default5c$pred,gbm_default6c$pred,gbm_default7c$pred,gbm_default8c$pred,
+               gbm_default9c$pred,gbm_default10c$pred,gbm_default11c$pred,gbm_default12c$pred)
+
+
+forecast=accumulate_model(forecast)
+
+
+##
+save(forecast,file="forecasts-samples/gbm_default/gbm_default-cpi1.rda")
