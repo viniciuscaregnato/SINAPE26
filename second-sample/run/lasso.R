20ac7b1a1a03c61bd6d6072da7f1829951c24a82
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2026-04-24 22:22:22 +0300

    Add files via upload

---
+++ second-sample/run/lasso.R
@@ -0,0 +1,38 @@
+library(HDeconometrics)
+load("second-sample/data/rawdata.RData")
+Y=dados
+dum=rep(0,nrow(Y))
+dum[which.min(Y[,1])]=1
+Y=cbind(Y,dum=dum)
+
+nprev=180
+alpha=1
+
+## == presente == ##
+
+lasso1c=lasso.rolling.window(Y,nprev,1,1,alpha,type="lasso")
+lasso2c=lasso.rolling.window(Y,nprev,1,2,alpha,type="lasso")
+lasso3c=lasso.rolling.window(Y,nprev,1,3,alpha,type="lasso")
+lasso4c=lasso.rolling.window(Y,nprev,1,4,alpha,type="lasso")
+lasso5c=lasso.rolling.window(Y,nprev,1,5,alpha,type="lasso")
+lasso6c=lasso.rolling.window(Y,nprev,1,6,alpha,type="lasso")
+lasso7c=lasso.rolling.window(Y,nprev,1,7,alpha,type="lasso")
+lasso8c=lasso.rolling.window(Y,nprev,1,8,alpha,type="lasso")
+lasso9c=lasso.rolling.window(Y,nprev,1,9,alpha,type="lasso")
+lasso10c=lasso.rolling.window(Y,nprev,1,10,alpha,type="lasso")
+lasso11c=lasso.rolling.window(Y,nprev,1,11,alpha,type="lasso")
+lasso12c=lasso.rolling.window(Y,nprev,1,12,alpha,type="lasso")
+
+
+
+
+### == juntando tudo ==  ###
+
+forecast=cbind(lasso1c$pred,lasso2c$pred,lasso3c$pred,lasso4c$pred,
+               lasso5c$pred,lasso6c$pred,lasso7c$pred,lasso8c$pred,
+               lasso9c$pred,lasso10c$pred,lasso11c$pred,lasso12c$pred)
+
+forecast=accumulate_model(forecast)
+
+##
+save(forecast, file = "forecasts-samples/lasso/lasso-cpi2.rda")
