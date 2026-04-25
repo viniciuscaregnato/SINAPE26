a06951d3550f2f97bb20f7b16e7cc73c31a887f8
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2026-04-25 15:39:57 -0300

    Refactor xgboost CV functions and add forecast accumulation
    Refactor xgboost cross-validation functions and add accumulation model for forecasts.

--- second-sample/functions/func-xgb-cv.R
+++ second-sample/functions/func-xgb-cv.R
@@ -333,3 +333,22 @@ xgb_cv.rolling.window=function(Y,nprev,indice=1,lag=1){
   
 }
 
+
+accumulate_model = function(forecasts){
+  
+  acc3 = c(rep(NA,2),sapply(1:(nrow(forecasts)-2), function(x){
+    prod(1+diag(forecasts[x:(x+2),1:3]))-1
+  })) 
+  acc6 = c(rep(NA,5),sapply(1:(nrow(forecasts)-5), function(x){
+    prod(1+diag(forecasts[x:(x+5),1:6]))-1
+  }))
+  acc12 = c(rep(NA,11),sapply(1:(nrow(forecasts)-11), function(x){
+    prod(1+diag(forecasts[x:(x+11),1:12]))-1
+  }))
+  
+  forecasts = cbind(forecasts,acc3,acc6,acc12)
+  colnames(forecasts) = c(paste("t+",1:12,sep = ""),"acc3","acc6","acc12")
+  
+  return(forecasts)
+  
+}
