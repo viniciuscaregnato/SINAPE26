95aa32094b542b4fd1676787daae7ac2ef82116b
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2026-04-25 16:29:44 -0300

    Update func-xgb-cv.R

--- second-sample/functions/func-xgb-cv.R
+++ second-sample/functions/func-xgb-cv.R
@@ -22,10 +22,10 @@ runxgb_cv=function(Y,indice,lag){
   }
   
   X.out=tail(aux,1)[,1:ncol(X), drop= FALSE] # y = y[1:(length(y)-lag+1)]
-  dum=tail(dum,length(y)); X = cbind(X,dum); X.out = cbind(X.out,0); colnames(X.out)= NULL; colnames(X) = NULL #  X = X[1:(nrow(X)-lag+1),]
+      dum=tail(dum,length(y)); X = cbind(X,dum); X.out = cbind(X.out,0); colnames(X.out)= NULL; colnames(X) = NULL #  X = X[1:(nrow(X)-lag+1),]
   
   # print("line 24")
-  
+
   
   
   dtrain <- xgb.DMatrix(X, label = y)
@@ -57,15 +57,7 @@ runxgb_cv=function(Y,indice,lag){
                    verbose = F, early_stopping_rounds = 8, maximize = FALSE)
     
     min_rmse_index  <-  mdcv$best_iteration
-    
-    # --- adicionar essa verificação ---
-    if (is.null(min_rmse_index) || min_rmse_index == 0) next
-    
-    
     min_rmse <-  mdcv$evaluation_log[min_rmse_index]$test_rmse_mean
-    
-    # --- e essa também ---
-    if (is.null(min_rmse) || length(min_rmse) == 0) next
     
     if (min_rmse < best_rmse) {
       best_rmse <- min_rmse
@@ -131,11 +123,11 @@ runxgb_cv=function(Y,indice,lag){
   
   
   return(list(#"model"= model,
-    "pred"= pred
-    # "pred_intervals" = pred_intervals#,
-    # "geweke_bart_testpreds"= geweke_bart_testpreds,
-    # "geweke_bart_sigma" = geweke_bart_sigma
-  ))
+              "pred"= pred
+              # "pred_intervals" = pred_intervals#,
+              # "geweke_bart_testpreds"= geweke_bart_testpreds,
+              # "geweke_bart_sigma" = geweke_bart_sigma
+              ))
 }
 
 
@@ -147,23 +139,197 @@ xgb_cv.rolling.window=function(Y,nprev,indice=1,lag=1){
   save.pred=matrix(NA,nprev,1)
   
   
-  for(i in 1:nprev){
-    cat("Iteração", i, "de", nprev, "\n")  # mostra progresso
+  # save.pred_intervals = matrix(NA,nprev,2)
+  
+  # save.geweke_bart_testpreds = matrix(NA,nprev,1)
+  # save.geweke_bart_sigma = matrix(NA,nprev,1)
+  
+  # i <- nprev
+  
+  #code for parallelization
+  
+  
+  
+  
+  #for(i in nprev:1){
+  myfunction <- function(i) {
+    library(xgboost)
     
-    Y.window = Y[(1+nprev-i):(nrow(Y)-i), ]
-    lasso = runxgb_cv(Y.window, indice, lag)
-    save.pred[(1+nprev-i), ] = lasso$pred
+    res_temp <- list()
+    
+    Y.window=Y[(1+nprev-i):(nrow(Y)-i),]
+    #cvinal medeiros et al. code uses "lasso" here to refer to all models
+    
+    # print("line 149")
+    lasso=runxgb_cv(Y.window,indice,lag)
+    
+    # print("line 152")
+    
+    
+    
+    #SAVE PREDICTIONS
+    #this should be included for all methods
+    res_temp$save.pred=lasso$pred
+    
+    #SAVE PREDICTION INTERVALS
+    #not all methods include prediciton intervals
+    #ensure that all prediction intervals are saved in the same format for all methods
+    # res_temp$save.pred_intervals=lasso$pred_intervals
+    
+    
+    #SAVE MCMC DIAGNOSTICS
+    #onlyapplicable to methods that use MCMC
+    
+    # res_temp$save.geweke_bart_testpreds=lasso$geweke_bart_testpreds
+    # res_temp$save.geweke_bart_sigma=lasso$geweke_bart_sigma
+    
+    
+    #SAVE VARIABLE IMPORTANCE RESULTS
+    #Note: This is specific to the methods
+    #Different methods have different variable importance measures
+    #some methods have no variable importance measures
+    #some methods have more than one variable importance measure
+    
+    # temp_varcounts <- lasso$model$varcount
+    #BART variable inclusion probabilities as defined by Chipman et al
+    #For each sum-of-tree model, obtain the proportions of splits for each variable
+    #then average this across mcmc draws of sum-of-tree models
+    
+    # res_temp$save.importance =colMeans((temp_varcounts/rowSums(temp_varcounts)))
+    
+    #the posterior inclusion probabilities are averages across model draws of 
+    #indicators for whether the variable was used for any splits in the sum-of-tree model
+    # res_temp$save.pip = colMeans(1*(temp_varcounts>0))
+    
+    #cat("iteration",(1+nprev-i),"\n")
+    
+    
+    return(res_temp)
+    
+    
   }
+  
+  
+  
+  #no_cores <- detectCores(logical = TRUE)  # returns the number of available hardware threads, and if it is FALSE, returns the number of physical cores
+  
+  cl <- makeCluster(120)
+  clusterSetRNGStream(cl = cl, iseed = 123)
+  
+  clusterExport(cl,c('myfunction',
+                        'nprev',
+                        'indice',
+                        'lag',
+                        'Y',
+                        'runxgb_cv'
+                        ),
+                envir = environment()
+                )
+  
+  #registerDoParallel(cl)
+  
+  
+  # start.time <- Sys.time()
+  
+  res_list <- parallel::parLapply(cl = cl, 1:nprev, fun = myfunction)
+  
+  stopCluster(cl)
+  
+  # end.time <- Sys.time()
+  # time.taken <- end.time - start.time
+  # time.taken
+  
+  for (i in nprev:1){
+    
+    save.pred[(1+nprev-i),]=res_list[[i]]$save.pred
+    # save.pred_intervals[(1+nprev-i),]=res_list[[i]]$save.pred_intervals
+    # save.geweke_bart_testpreds[(1+nprev-i),]=res_list[[i]]$save.geweke_bart_testpreds
+    # save.geweke_bart_sigma[(1+nprev-i),]=res_list[[i]]$save.geweke_bart_sigma
+    # save.importance[[i]] =res_list[[i]]$save.importance
+    # save.pip[[i]] = res_list[[i]]$save.pip
+    
+    
+  }
+  
+  
   
   
   real=Y[,indice]
   
+  #these lines seem pointless, commenting out
+  # plot(real,type="l")
+  # lines(c(rep(NA,length(real)-nprev),save.pred),col="red")
+  
+  rmse=sqrt(mean((tail(real,nprev)-save.pred)^2))
+  mae=mean(abs(tail(real,nprev)-save.pred))
+  
+  #median absolute deviation from the median in paper, but not in code
+  mad = median(abs(tail(real,nprev)-save.pred - median(tail(real,nprev)-save.pred)))
+  
+  #mean absolute deviation from  he mean
+  mean_ad = mean(abs(tail(real,nprev)-save.pred - mean(tail(real,nprev)-save.pred)))
+
+  
+  #mean relative absolute error (relative to random walk)
+  #last 132 lagged one month values are
+  #real[(nrow(dados)-nprev):(nrow(dados)-1)]
+  mrae = mean(abs( (tail(real,nprev)-save.pred)/
+                        (tail(real,nprev)-real[(nrow(dados)-nprev):(nrow(dados)-1)]  )     ))
+  
+  #mean absolute scaled error
+  #equivalent to mae of method divided by nae of naive forecast
+  #first calculate the denomiator
+  #mean of vector of length nprev-1
+  tempdenom = mean( abs(tail(real,nprev-1) - real[(nrow(dados)-nprev+1):(nrow(dados)-1)]  ) )
+  #then the overall measure is
+  mase=mae/tempdenom
+  
+  #mean absolute percentage error
+  mape = (100/nprev)*mean(abs((tail(real,nprev)-save.pred)/tail(real,nprev)))
+  
+  #normalized rmse
+  nrmse = rmse/(max(tail(real,nprev))-min(tail(real,nprev)))
+  
+  #rmse relative to random walk
+  
+  #rmse of naive rw forecast
+  rwrmse=sqrt(mean((tail(real,nprev)-real[(nrow(dados)-nprev):(nrow(dados)-1)])^2))
+  
+  rmse_rel_rw = rmse/rwrmse
+  
+  
+  errors=c("rmse"=rmse,
+           "mae"=mae,
+           "mad"=mad,
+           "mean_ad"=mean_ad,
+           "mrae"=mrae,
+           "mase"=mase,
+           "mape"=mape,
+           "nrmse"=nrmse,
+           "rmse_rel_rw"=rmse_rel_rw)
+  
+  #mean prediction interval coverage
+  # predint_cov = mean( 1*((save.pred_intervals[,1] < tail(real,nprev))  & (tail(real,nprev) < save.pred_intervals[,2])   ))
+  
+  #mean prediction interval width
+  # predint_width = mean(save.pred_intervals[,2] - save.pred_intervals[,1])
   
   
   
+
   
   
-  return(list("pred"= save.pred))
+  
+  return(list("pred"= save.pred,
+              "errors"= errors#,
+              # "save.pred_intervals"= save.pred_intervals,
+              #"save.geweke_bart_testpreds"= save.geweke_bart_testpreds,
+              #"save.geweke_bart_sigma"= save.geweke_bart_sigma,
+              # "save.importance"= save.importance,
+              # "save.pip"= save.pip#,
+              # "predint_cov"= predint_cov,
+              # "predint_width"= predint_width
+              ))
   
 }
 
